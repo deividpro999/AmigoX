ad7da77956120b25270fb3030e0fa38dbc5cdb67
deividpro999 <102329898+deividpro999@users.noreply.github.com>
2025-04-22 03:45:30 -0300

    Delete AmigoX/pages/index.js

--- AmigoX/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Bem-vindo ao AmigoX ??</h1>
-      <p>Vamos sortear quem tirou quem no amigo secreto!</p>
-    </div>
-  );
-}
